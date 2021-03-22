ab7c6195a5e20dfbe9cf9120b476aff435814216
Umair Saeed <461339@student.fontys.nl>
2021-03-22 11:42:00 +0100

    1e versie van de bestanden

---
+++ index.php
@@ -0,0 +1,38 @@
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
+		<link rel="stylesheet" href="styles/style.css">
+		<title>Home</title>
+	</head>
+	<body>
+		<aside>
+			<nav  id="mySidenav" class="sidenav">
+				<a href="#hero" id="home"><i class="fa fa-home"></i>Home</a>
+				<a href="#hero" id="about"><i class="fas fa-address-card"></i>About</a>
+				<a href="#hero" id="folio"><i class="fas fa-briefcase"></i>Portfolio</a>
+				<a href="#contact" id="contact"><i class="fas fa-at"></i>Contact</a>
+			</nav>
+		</aside>
+		<nav class="mobile-nav">
+			<a href="#home">Home</a>
+			<a href="#about">About</a>
+			<a href="#folio">Folio</a>
+			<a href="#contact">Contact</a>
+		</nav>
+		<section id="hero">
+			<div class="container">
+			<div class="right-section">
+				<div class="right-section-wrapper">
+					<h1 class="title">Umair Saeed</h1>
+					<h3 class="description">
+						See how experienced developers solve problems in real-time. 
+					</h3>
+				</div>
+			</div>
+		</section>
+		<script src="js/main.js"></script>
+	</body>
+</html>
